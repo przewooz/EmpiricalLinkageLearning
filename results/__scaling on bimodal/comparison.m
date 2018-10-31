a312a2be0d9420061cec01eb24a4e085e58dd671
Michal Przewozniczek <michal.przewozniczek@pwr.edu.pl>
2018-10-31 16:14:38 +0100

    no message

---
+++ results/__scaling on bimodal/comparison.m
@@ -0,0 +1,13 @@
+[r_e] = ranksum(a3LOa_50_ffe_t, a3LOa_5_ffe_2_t);
+r_e
+%[r_mup] = ranksum(sGAunif06, MSF, 'TAIL', 'right');
+%r_mup
+%[r_ts] = ranksum(sGAunif06, MSF, 'TAIL', 'left');
+%r_ts*/
+
+%[s_e] = signtest(worst_p3, worst_cplex2);
+%s_e
+%[s_dyn] = signtest(worst_p3, worst_cplex2, 'TAIL', 'right');
+%s_dyn
+%[s_st] = signtest(zmin_1_p3_dyn, zmin_1_p3_st, 'TAIL', 'left');
+%s_st
